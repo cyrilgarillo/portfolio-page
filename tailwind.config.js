94e74a980ed996034702f06207b0d10711771989
cyrilgarillo <165701680+cyrilgarillo@users.noreply.github.com>
2024-10-06 10:03:55 +0200

    Added Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Stelle sicher, dass hier auf alle Angular-Dateien verwiesen wird
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
