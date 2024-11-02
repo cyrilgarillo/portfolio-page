ba56fe4a013988eefe27edf46b0132263af459cc
cyrilgarillo <165701680+cyrilgarillo@users.noreply.github.com>
2024-11-02 09:19:16 +0100

    Home + Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'portfolio', component: PortfolioComponent }
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'pages', component: PagesComponent }
 ];
 
 @NgModule({
